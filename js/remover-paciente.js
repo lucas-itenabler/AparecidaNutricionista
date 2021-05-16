1a36fb20606322f58892fa6cc620abfdc3d2bef9
Lucas Santana <lsantana.2015@alunos.utfpr.edu.br>
2021-05-16 00:12:21 -0300

    Criando uma classe para remoção do paciente

---
+++ js/remover-paciente.js
@@ -0,0 +1,9 @@
+var paciente = document.querySelectorAll(".paciente");
+
+pacientes.forEach(function(paciente){
+
+    paciente.addEventListener("dblclick", function(){
+        this.remove();
+    });
+
+});
